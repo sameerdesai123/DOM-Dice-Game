bcb1980d6506a564a1bc2764ad856086e9a0bec8
Sameer Desai <48178078+sameerdesai123@users.noreply.github.com>
2019-07-21 20:01:23 +0530

    Update challenges.js

--- challenges.js
+++ challenges.js
@@ -1,12 +1,3 @@
-/*
-YOUR 3 CHALLENGES
-Change the game to follow these rules:
-
-1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
-2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
-3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
-*/
-
 var scores, roundScore, activePlayer, gamePlaying;
 
 init();
@@ -34,23 +25,6 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
             //Next player
             nextPlayer();
         }
-        
-        /*
-        if (dice === 6 && lastDice === 6) {
-            //Player looses score
-            scores[activePlayer] = 0;
-            document.querySelector('#score-' + activePlayer).textContent = '0';
-            nextPlayer();
-        } else if (dice !== 1) {
-            //Add score
-            roundScore += dice;
-            document.querySelector('#current-' + activePlayer).textContent = roundScore;
-        } else {
-            //Next player
-            nextPlayer();
-        }
-        lastDice = dice;
-        */
     }    
 });
 
